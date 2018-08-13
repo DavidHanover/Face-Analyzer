83529246c01d11c5115ad0d207f700fed1759142
DavidHanover <DavidHanover89@gmail.com>
2018-08-12 18:27:05 -0700

    final testing message hopefully

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
       <h1>TEST</h1>   
-      <p> This was all just a test</p>     
+      <p> This was all just a test. No more testing!  Time to start dammit.</p>     
       </div>
     );
   }
