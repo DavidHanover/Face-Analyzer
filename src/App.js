d19d173c017660cc12eb1dd9d364ae3a5c7db997
DavidHanover <31142178+DavidHanover@users.noreply.github.com>
2018-08-12 18:29:22 -0700

    removed testing messages

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>TEST</h1>   
-      <p> This was all just a test. No more testing!  Time to start dammit.</p>     
+         
       </div>
     );
   }
