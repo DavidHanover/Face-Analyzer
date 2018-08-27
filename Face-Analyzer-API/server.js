d5363fb5d38b23457014aec9a676d22dbeffb4da
DavidHanover <DavidHanover89@gmail.com>
2018-08-27 00:03:27 -0700

    deleted pointless line for optimization purposes

--- Face-Analyzer-API/server.js
+++ Face-Analyzer-API/server.js
@@ -8,8 +8,8 @@ const postgres = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'postgres',
-    password : 'funk10162',
+    user : '',
+    password : '',
     database : 'face-analyzer'
   }
 });
