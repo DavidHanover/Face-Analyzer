09f1eff9e5f4e0db6753bd2f7835b91cbbf30e12
DavidHanover <DavidHanover89@gmail.com>
2018-08-27 00:00:01 -0700

    imported bcrypt-nodejs and knex, connected knex to DB, now have server console logging a simple query

--- Face-Analyzer-API/server.js
+++ Face-Analyzer-API/server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const postgres = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'funk10162',
+    database : 'face-analyzer'
+  }
+});
+
+console.log(postgres.select('*').from('users'));
 
 const app = express();
 app.use(cors()); 
